02f6a1c789ddf566bcef6f49e0bbf04e5255eb17
David Griffiths <davidgg777@hotmail.com>
2022-04-04 19:37:43 +1000

    adding deal_stage

--- models/warehouse/fct_deal.sql
+++ models/warehouse/fct_deal.sql
@@ -8,6 +8,43 @@ with deal as (
     from {{ ref('int_deal_stage') }}
 )
 
-, joined as (
-    
+, deal_stage_agg as (
+    select deal_id, 
+        sum(duration_in_stage_seconds) as age_deal_seconds
+    from deal_stage
+    group by 1
 )
+
+, deal_stage_grouped as (
+    select
+        deal_id,
+        deal_stage_name,
+        sum(duration_in_stage_seconds) as duration_in_stage_seconds
+    from deal_stage
+    group by 1,2
+)
+
+, deal_stage_pivoted as (
+    select
+        deal_id,
+        {{ dbt_utils.pivot('deal_stage_name', 'duration_in_stage_seconds', prefix='seconds_in_') }}
+    from deal_stage_grouped
+)
+
+, joined as (
+    select
+        deal.*,
+        deal_stage_agg.age_deal_seconds,
+        deal_stage_pivoted.*
+    from deal
+    left join deal_stage_agg using (deal_id)
+    left join deal_stage_pivoted using (deal_id)
+)
+
+, final as (
+    select *
+    from joined
+)
+
+select *
+from final
--- models/warehouse/int/int_deal_stage.sql
+++ models/warehouse/int/int_deal_stage.sql
@@ -7,11 +7,18 @@ with deal_stage as (
 
     select
         deal_stage.deal_id || '-' || row_number() over(partition by deal_stage.deal_id order by deal_stage.date_entered asc) as deal_stage_id,
+        deal_stage.deal_id,
         deal_stage.deal_stage_name, 
         deal_stage._fivetran_start as date_stage_entered,
         deal_stage._fivetran_end as date_stage_exited,
         deal_stage._fivetran_active as is_stage_active,
-        {{dbt_utils.datediff(date_stage_exited, date_stage_exited, 'day')}} as duration_in_stage
+
+        case when  deal_stage_name = 'closedwon' then date_stage_entered
+          when IS_STAGE_ACTIVE then current_date 
+          else date_stage_exited end 
+        as date_stage_latest,
+        
+        {{dbt_utils.datediff('date_stage_entered', 'date_stage_latest', 'seconds')}} as duration_in_stage_seconds
 
     from deal_stage
 
